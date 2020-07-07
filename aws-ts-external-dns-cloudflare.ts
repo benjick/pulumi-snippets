42b7f52bf73df5daa7bf19a4f7075c6191a56197
Max Malm <benjick@dumfan.net>
2020-07-07 23:11:03 +0200

    Create aws-ts-external-dns-cloudflare.ts

---
+++ aws-ts-external-dns-cloudflare.ts
@@ -0,0 +1,41 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as k8s from '@pulumi/kubernetes';
+import * as aws from '@pulumi/aws';
+import { cluster } from './cluster';
+import { servicesNamespace } from './services-ns';
+
+const current = pulumi.output(aws.getCallerIdentity({ async: true }));
+export const accountId = current.accountId;
+
+const config = new pulumi.Config();
+const baseDomain = config.require('baseDomain');
+const apiToken = config.requireSecret('cfToken');
+const email = config.requireSecret('email');
+
+export const externalDnsChart = new k8s.helm.v2.Chart(
+  'external-dns',
+  {
+    chart: 'external-dns',
+    version: '3.2.3',
+    namespace: servicesNamespace.metadata.name,
+    values: {
+      provider: 'cloudflare',
+      cloudflare: {
+        apiToken,
+        email,
+        proxied: true, // enable the Cloudflare proxy feature
+      },
+      txtOwnerId: 'safira-external-dns-pulumi',
+      domainFilters: [baseDomain],
+      podSecurityContext: {
+        fsGroup: 65534,
+      },
+    },
+    fetchOpts: {
+      repo: 'https://charts.bitnami.com/bitnami',
+    },
+  },
+  {
+    provider: cluster.provider,
+  },
+);
