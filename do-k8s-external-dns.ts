46c843ef49fcb320f97d028232cb97444a0a1b78
Max Malm <benjick@dumfan.net>
2020-08-06 18:31:36 +0200

    Create do-k8s-external-dns.ts

---
+++ do-k8s-external-dns.ts
@@ -0,0 +1,30 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as k8s from '@pulumi/kubernetes';
+import { k8sProvider } from './k8s-provider';
+
+const config = new pulumi.Config('digitalocean');
+const apiToken = config.requireSecret('token');
+
+export const externalDnsChart = new k8s.helm.v2.Chart(
+  'external-dns',
+  {
+    chart: 'external-dns',
+    version: '3.2.3',
+    values: {
+      digitalocean: {
+        apiToken,
+      },
+      provider: 'digitalocean',
+      txtOwnerId: 'external-dns-pulumi',
+      podSecurityContext: {
+        fsGroup: 65534,
+      },
+    },
+    fetchOpts: {
+      repo: 'https://charts.bitnami.com/bitnami',
+    },
+  },
+  {
+    provider: provider,
+  },
+);
